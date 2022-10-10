336cca2d3a13f7ae4ad3eaba2f679ae1735bd52c
CarageaAlexandru <caragea.alexandru@yahoo.com>
2022-10-10 18:44:26 +0100

    added GET /api/users functionality and error handling

--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,6 @@ const { fetchAllUsers } = require("../models/users");
 module.exports.getAllUsers = (request, response, next) => {
 	fetchAllUsers()
 		.then((users) => {
-			console.log(users);
 			response.status(200).send({ users });
 		})
-		.catch((error) => {
-			console.log(error);
-		});
 };

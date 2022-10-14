a2a12a87f7276620176e9996461e15f2a31936a8
CarageaAlexandru <caragea.alexandru@yahoo.com>
2022-10-14 21:53:50 +0100

    added functionality for GET /api/users/username

--- __tests__/error-handling/users-endpoint.test.js
+++ __tests__/error-handling/users-endpoint.test.js
@@ -16,7 +16,18 @@ afterAll(() => {
 });
 
 describe("3. GET /api/users", () => {
-    test("should respond with status 404 if endpoint is not correct", () => {
-        return request(app).get("/api/usersWrong").expect(404);
-    });
+	test("should respond with status 404 if endpoint is not correct", () => {
+		return request(app).get("/api/usersWrong").expect(404);
+	});
+});
+describe("17.GET /api/users/:username", () => {
+	test("should respond with status 404 username is not found. ", () => {
+		return request(app)
+			.get("/api/users/99999fdwas")
+			.expect(404)
+			.then(({ body }) => {
+				const { message } = body;
+				expect(message).toBe("Username not found in database.");
+			});
+	});
 });

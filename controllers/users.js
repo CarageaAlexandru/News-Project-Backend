5f99925dc8725fa8976ada1a199a42b1350307fd
CarageaAlexandru <caragea.alexandru@yahoo.com>
2022-10-14 21:55:58 +0100

    added functionality for GET /api/users/username

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,22 @@
-const { fetchAllUsers } = require("../models/users");
+const { fetchAllUsers, fetchUserByUsername } = require("../models/users");
 
 module.exports.getAllUsers = (request, response, next) => {
 	fetchAllUsers()
 		.then((users) => {
 			response.status(200).send({ users });
 		})
+		.catch((error) => {
+			next(error);
+		});
+};
+
+module.exports.getUserByUsername = (request, response, next) => {
+	const { username } = request.params;
+	fetchUserByUsername(username)
+		.then((user) => {
+			response.status(200).send({ user });
+		})
+		.catch((error) => {
+			next(error);
+		});
 };
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,6 +1,7 @@
-const { getAllUsers } = require("../controllers/users");
+const { getAllUsers, getUserByUsername } = require("../controllers/users");
 const router = require("express").Router();
 
 router.get("/api/users", getAllUsers);
+router.get("/api/users/:username", getUserByUsername);
 
 module.exports = router;
